70acf85e3350dc4d1f7de90ed1c9cf4125fd8557
Dan Evron <dan.evron@klarna.com>
2018-03-16 01:25:42 +0200

    WIP, trying to reduce first job output file size by moving the first job reducer work into the second job mapper and remove the tags from the first job output

---
+++ take_two/FiratStep.java
@@ -0,0 +1,82 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class FiratStep {
+
+	/*
+	input:
+		facebook	videos
+		google	videos
+		google	search
+		facebook	photos
+		youtube	photos
+		youtube	videos
+
+	output:
+		videos	[facebook, google, youtube]
+		search	[google]
+		photos	[facebook, youtube]
+	*/
+	public static class FiratStepMapper extends
+	Mapper<Object, Text, Text, Text> {
+		public void map(Object key, Text value, Context context)
+		throws IOException, InterruptedException {
+			String record = value.toString();
+			String[] parts = record.split("\t");
+			context.write(new Text(parts[1]), new Text(parts[0]));
+		}
+	}
+
+	/*
+	input:
+		videos	[facebook, google, youtube]
+		search	[google]
+		photos	[facebook, youtube]
+
+	output:
+		facebook google youtube
+		google
+		facebook youtube
+	*/
+	public static class FiratStepReducer extends
+	Reducer<Text, Text, Text, NullWritable> {
+		public void reduce(Text key, Iterable<Text> values, Context context)
+		throws IOException, InterruptedException {
+			String value = "";
+
+			for (Text t : values) {
+				value += t.toString();
+				value += " ";
+			}
+
+			context.write(new Text(value), NullWritable.get());
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "FiratStep");
+		job.setJarByClass(FiratStep.class);
+		job.setReducerClass(FiratStepReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		MultipleInputs.addInputPath(job, new Path(args[0]),TextInputFormat.class, FiratStepMapper.class);
+		Path outputPath = new Path(args[1]);
+
+		FileOutputFormat.setOutputPath(job, outputPath);
+		outputPath.getFileSystem(conf).delete(outputPath);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
